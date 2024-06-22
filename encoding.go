cd6d0707aa0eb98f4655e84026eecfadb3420c8a
rob <rob@b7c.io>
2024-06-22 21:23:53 +1200

    add shockwave encoding

---
+++ encoding.go
@@ -0,0 +1,58 @@
+package goearth
+
+import (
+	"math/bits"
+)
+
+// Shockwave encoding
+
+func vl64len(v int) int {
+	if v < 0 {
+		v *= -1
+	}
+	return (bits.Len32(uint32(v)) + 9) / 6
+}
+
+func vl64encode(b []byte, v int) {
+	abs := v
+	if abs < 0 {
+		abs *= -1
+	}
+	n := vl64len(v)
+
+	b[0] = 64 | (byte(n)&7)<<3 | byte(abs&3)
+	if v < 0 {
+		b[0] |= 4
+	}
+	for i := 1; i < n; i++ {
+		b[i] = 64 | byte((abs>>(2+6*(i-1)))&0x3f)
+	}
+}
+
+func vl64decode(b []byte) int {
+	value := int(b[0] & 3)
+
+	n := int(b[0]>>3) & 7
+	for i := 1; i < n; i++ {
+		value |= int(b[i]&0x3f) << (2 + 6*(i-1))
+	}
+
+	if b[0]&4 != 0 {
+		value *= -1
+	}
+	return value
+}
+
+func b64encode(b []byte, v int) {
+	for i := 0; i < len(b); i++ {
+		b[i] = 64 | byte(v>>((len(b)-i-1)*6)&0x3f)
+	}
+}
+
+func b64decode(b []byte) int {
+	v := 0
+	for i := 0; i < len(b); i++ {
+		v |= int(b[i]&0x3f) << ((len(b) - i - 1) * 6)
+	}
+	return v
+}
---
+++ encoding_test.go
@@ -0,0 +1,114 @@
+package goearth
+
+import (
+	"testing"
+)
+
+// value -> expected length
+var vl64len_tests = map[int]int{
+	0:        1,
+	1:        1,
+	-1:       1,
+	2:        1,
+	-2:       1,
+	3:        1,
+	-3:       1,
+	4:        2,
+	-4:       2,
+	128:      2,
+	-128:     2,
+	255:      2,
+	-255:     2,
+	256:      3,
+	-256:     3,
+	8192:     3,
+	-8192:    3,
+	16383:    3,
+	-16383:   3,
+	16384:    4,
+	-16384:   4,
+	1048575:  4,
+	-1048575: 4,
+	1048576:  5,
+	-1048576: 5,
+}
+
+// value -> encoded
+var vl64_tests = map[int]string{
+	0:        "H",
+	1:        "I",
+	2:        "J",
+	3:        "K",
+	4:        "PA",
+	64:       "PP",
+	250:      "R~",
+	256:      "X@A",
+	-256:     "\\@A",
+	1024:     "X@D",
+	16384:    "`@@A",
+	-16384:   "d@@A",
+	1048576:  "h@@@A",
+	-1048576: "l@@@A",
+}
+
+// value -> encoded
+var b64_tests = map[int]string{
+	0:    "@@",
+	1:    "@A",
+	16:   "@P",
+	256:  "D@",
+	1337: "Ty",
+	2048: "`@",
+	4000: "~`",
+}
+
+func TestVL64len(t *testing.T) {
+	for value, expected := range vl64len_tests {
+		actual := vl64len(value)
+		if actual != expected {
+			t.Fatalf("vl64len(%d) was %d, expected %d", value, actual, expected)
+		}
+	}
+}
+
+func TestVL64encode(t *testing.T) {
+	for value, expected := range vl64_tests {
+		length := vl64len(value)
+		buf := make([]byte, length)
+		vl64encode(buf, value)
+		actual := string(buf)
+		if actual != expected {
+			t.Fatalf("vl64encode(%d) was %q, expected %q", value, actual, expected)
+		}
+	}
+}
+
+func TestVL64decode(t *testing.T) {
+	for expected, encoded := range vl64_tests {
+		buf := []byte(encoded)
+		actual := vl64decode(buf)
+		if actual != expected {
+			t.Fatalf("vl64decode(%q) was %d, expected %d", encoded, actual, expected)
+		}
+	}
+}
+
+func TestB64encode(t *testing.T) {
+	for value, expected := range b64_tests {
+		buf := make([]byte, 2)
+		b64encode(buf, value)
+		actual := string(buf)
+		if actual != expected {
+			t.Fatalf("b64encode(%d) was %q, expected %q", value, actual, expected)
+		}
+	}
+}
+
+func TestB64decode(t *testing.T) {
+	for expected, encoded := range b64_tests {
+		actual := b64decode([]byte(encoded))
+		if actual != expected {
+			t.Fatalf("b64decode(%q) was %d, expected %d", encoded, actual, expected)
+		}
+	}
+}
