298878b2a1e80e0f2c3db63f7bdcca38dee71835
rob <rob@b7c.io>
2024-06-25 23:05:03 +1200

    add inventory manager

---
+++ shockwave/inventory/inventory.go
@@ -0,0 +1,40 @@
+package inventory
+
+import (
+	g "xabbo.b7c.io/goearth"
+)
+
+// Inventory represents a list of inventory items.
+type Inventory struct {
+	Items []Item
+}
+
+func (inv *Inventory) Parse(p *g.Packet, pos *int) {
+	*inv = Inventory{}
+	p.Read(&inv.Items)
+}
+
+// Item represents an inventory item.
+type Item struct {
+	ItemId int
+	Pos    int
+	// Type represents the type of the item.
+	// May be "S" for "stuff" (floor item), or "I" for "item" (wall item).
+	Type       string
+	Id         int
+	Class      string
+	DimX, DimY int
+	Colors     string
+	Props      string
+}
+
+func (item *Item) Parse(p *g.Packet, pos *int) {
+	*item = Item{}
+	p.ReadPtr(pos, &item.ItemId, &item.Pos, &item.Type, &item.Id, &item.Class)
+	switch item.Type {
+	case "S":
+		p.Read(&item.DimX, &item.DimY, &item.Colors)
+	case "I":
+		p.Read(&item.Props)
+	}
+}
---
+++ shockwave/inventory/inventory_manager.go
@@ -0,0 +1,60 @@
+package inventory
+
+import (
+	g "xabbo.b7c.io/goearth"
+	"xabbo.b7c.io/goearth/internal/debug"
+	"xabbo.b7c.io/goearth/shockwave/in"
+	"xabbo.b7c.io/goearth/shockwave/out"
+)
+
+var dbg = debug.NewLogger("[inventory]")
+
+// Manager tracks the state of the inventory.
+type Manager struct {
+	ext         *g.Ext
+	updated     g.VoidEvent
+	itemRemoved g.Event[ItemArgs]
+	items       map[int]Item
+}
+
+// NewManager creates a new inventory Manager using the provided extension.
+func NewManager(ext *g.Ext) *Manager {
+	mgr := &Manager{
+		ext:   ext,
+		items: map[int]Item{},
+	}
+	ext.Intercept(in.STRIPINFO_2).With(mgr.handleStripInfo2)
+	ext.Intercept(in.REMOVESTRIPITEM).With(mgr.handleRemoveStripItem)
+	return mgr
+}
+
+// Items returns the inventory items.
+func (mgr *Manager) Items() map[int]Item {
+	return mgr.items
+}
+
+// Update sends a request to retrieve the user's inventory.
+func (mgr *Manager) Update() {
+	mgr.ext.Send(out.GETSTRIP)
+}
+
+func (mgr *Manager) handleStripInfo2(e *g.InterceptArgs) {
+	var inv Inventory
+	e.Packet.Read(&inv)
+	for _, item := range inv.Items {
+		mgr.items[item.ItemId] = item
+	}
+	mgr.updated.Dispatch()
+
+	dbg.Printf("added %d items", len(inv.Items))
+}
+
+func (mgr *Manager) handleRemoveStripItem(e *g.InterceptArgs) {
+	itemId := e.Packet.ReadInt()
+	if item, ok := mgr.items[itemId]; ok {
+		delete(mgr.items, itemId)
+		mgr.itemRemoved.Dispatch(&ItemArgs{item})
+	} else {
+		dbg.Printf("failed to remove item (id: %d)", itemId)
+	}
+}
